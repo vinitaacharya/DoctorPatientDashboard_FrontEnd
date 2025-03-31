62f9123dbdad22348b64eeb56b93e76c6cd3e22b
vinitaacharya <vinitaacharya2004@gmail.com>
2025-03-31 15:41:52 -0400

    dashboard outline and nav bar fix

--- src/patient_dashboard/patient_landing.js
+++ src/patient_dashboard/patient_landing.js
@@ -1,14 +1,71 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; 
 import Patient_Navbar from "./patient_navbar"; 
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import { styled } from '@mui/material/styles';
+import FlowerBackgroundImg from "./patient_landing_assets/FlowerBackground.png"
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  borderRadius:30,
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  padding: 183,
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 function Patient_Landing() {
   return (
-    <>
+    <div style={{ display: "flex" }}>
+      {/* Sidebar/Navbar */}
       <Patient_Navbar /> 
-      <h1></h1>
-      
-    </>
+
+      <div style={{ marginLeft: "3px", flexGrow: 1, padding: "20px" }}>
+        <Box sx={{ flexGrow: 1 }}>
+          <Grid container spacing={2} >
+            <Grid item xs={7}>
+              <Item sx={{background: "linear-gradient(110deg, #5889BD 6.67%, #719EC7 34.84%, #99C6DB 93.33%)"}}>xs=7</Item>
+            </Grid>
+            <Grid item xs={5}>
+              <Item sx={{backgroundColor:"#EEF2FE"}}>xs=5</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item sx={{background: "linear-gradient(110deg, #5889BD 6.67%, #719EC7 34.84%, #99C6DB 93.33%)"}}>xs=4</Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item  sx={{ 
+    position: "relative",
+    overflow:"hidden",
+    backgroundImage: `url(${FlowerBackgroundImg})`, 
+    backgroundColor: "lightgray", 
+    backgroundSize: "cover",
+    backgroundPosition: "center", 
+    backgroundRepeat: "no-repeat",
+    "&::before": { 
+      content: '""',
+      position: "absolute",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      background: "rgba(255, 255, 255, 0.1)", 
+      backdropFilter: "blur(10px)", 
+      zIndex: 1,
+    }
+  }}><Box sx={{ position: "relative", zIndex: 2 }}>xs=4</Box></Item>
+            </Grid>
+            <Grid item xs={4}>
+              <Item sx={{background: "linear-gradient(110deg, #5889BD 6.67%, #719EC7 34.84%, #99C6DB 93.33%)", backgroundSize: "cover",}}>xs=4</Item>
+            </Grid>
+          </Grid>
+        </Box>
+      </div>
+    </div>
   );
 }
 
--- src/patient_dashboard/patient_navbar.js
+++ src/patient_dashboard/patient_navbar.js
@@ -1,26 +1,55 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Drawer, List, ListItemButton, ListItemIcon, Avatar, Box, Divider } from "@mui/material";
-import HomeIcon from "@mui/icons-material/Home";
-import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
-import PersonIcon from "@mui/icons-material/Person";
 import ClipBoardImg from "./nav_assets/Clipboard.png";
 import DollarSignImg from "./nav_assets/Dollar sign.png";
 import HeartImg from "./nav_assets/Heart.png";
 import HomeImg from "./nav_assets/Home.png";
 import UsersImg from "./nav_assets/Users.png";
 import ProfileImg from "./nav_assets/Profile.png"
+import LogoutImg from "./nav_assets/logout.png"
+
 
 const sidebarItems = [
   { icon:<img src={HeartImg} className="heart" style={{ width:50}}/>, text: "Heart Chart"  },
+  { icon:<img src={LogoutImg} className="heart" style={{ width:50}}/>, text: "Log Out"  },
   { icon:<img src={HomeImg} className="home_button" style={{width:37}}/>, text: "Medical Chart" },
   { icon:<img src={UsersImg} className="community_button" style={{width:37}} />, text: "Medical Chart" },
   { icon:<img src={ClipBoardImg} className="medical_chart_button" style={{width:37}}/>, text: "Medical Chart" },
   { icon:<img src={DollarSignImg} className="expenses_button" style={{width:37}}/>, text: "Medical Chart" },
   { icon:<Avatar alt="Remy Sharp" src={ProfileImg} style={{width:65, height:65}} />},];
-  
+
+
+
+
 
 export default function Patient_Navbar() {
+  const navigate = useNavigate();
+  //direct to designated page
+  const handleClick = (index) => {
+    console.log('clicked with parameter:', index);
+    
+    if (index === 1) {
+      navigate("/landing");
+    }
+    if (index === 2) {
+      navigate("/patient_dashboard/patient_landing");
+
+    }
+    if (index === 3) {
+      //enter path here - community page
+    }
+    if (index === 4) {
+      //enter path here - medical charts
+    }
+    if (index === 5) {
+      //enter path here - billing
+    }
+    if (index === 6) {
+      //enter path here - community page profile
+    }
+  };
   return (
     <Drawer
       variant="permanent"
@@ -28,8 +57,8 @@ export default function Patient_Navbar() {
         width: 90,
         flexShrink: 0,
         "& .MuiDrawer-paper": {
-          width: 95,
-          backgroundColor: "#E8F0FF", // Light blue background
+          width: 97,
+          //backgroundColor: "#E8F0FF", // Light blue background
           display: "flex",
           alignItems: "center",
           paddingTop: 2,
@@ -41,16 +70,16 @@ export default function Patient_Navbar() {
         <List>
           {sidebarItems.map((item, index) => (
             <React.Fragment key={index}>
-            <ListItemButton key={index} sx={{ display: "flex", justifyContent: "center", marginY: 1}}>
+            <ListItemButton key={index} sx={{ display: "flex", justifyContent: "center", marginY: 1}} onClick={() => handleClick(index)}>
               <ListItemIcon sx={{ color: "#345995", minWidth: "unset" }}>{item.icon}</ListItemIcon>
             </ListItemButton>
-            {index == 0 && (
-             <div style={{height:120 }}></div>
+            {index == 1 && (
+             <div style={{height:100, width:9 }}></div>
             )}
-            {index == 4 && (
-             <div style={{height:150 }}></div>
+            {index == 5 && (
+             <div style={{height:120, width:9 }}></div>
             )}
-            {index == 4 || index == 0 || index !=sidebarItems.length-1 && (
+            {index ==0 ||index == 5 || index == 1 || index !=sidebarItems.length-1 && (
               <Divider sx={{width:"50%", marginY:2, marginLeft:3}}/>
             )}
 
