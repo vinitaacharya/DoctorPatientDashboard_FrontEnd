103760e442f1d491ef46a26f778e179b1ce96f44
Xitlaly-P <jada23dem@gmail.com>
2025-04-10 15:51:00 -0400

    pharmacy landing base

--- src/pharmacy/pharmacy_landing.js
+++ src/pharmacy/pharmacy_landing.js
@@ -4,6 +4,7 @@ import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { styled } from '@mui/material/styles';
+import { Typography } from "@mui/material";
 
 
 const Item = styled(Paper)(({ theme }) => ({
@@ -16,9 +17,9 @@ const Item = styled(Paper)(({ theme }) => ({
     //padding: theme.spacing(2),
     color: (theme.vars ?? theme).palette.text.secondary,
     ...theme.applyStyles('dark', {
-      backgroundColor: '#1A2027',
+        backgroundColor: '#1A2027',
     }),
-  }));
+}));
 
 const style = {
     position: 'absolute',
@@ -35,7 +36,14 @@ const style = {
 function Pharmacy_Landing() {
     return (
         <div style={{ display: "flex" }}>
-          <Pharmacy_Navbar />
+            <Pharmacy_Navbar />
+            <div style={{ marginLeft: "3px", flexGrow: 1, padding: "20px" }}>
+                <Box sx={{ flexGrow: 1, p: 3, backgroundColor: "#EEF2FE", height: "90vh", marginTop: "5vh", paddingTop: "0px", paddingLeft: 0, paddingRight:0, borderTopLeftRadius: 30, borderTopRightRadius: 30, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, width: "85vw", margin: 'auto'}}>
+                <Typography variant="h4" sx={{ fontWeight: 'bold', color: '#FFF', fontFamily: "Montserrat", width: "100%", backgroundColor:"#5889BD", borderTopLeftRadius: 30, borderTopRightRadius: 30, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, paddingBottom: '1vh', paddingTop: '1vh'}}>
+                    Stock Inventory
+                </Typography>
+                </Box>
+            </div>
         </div>
     )
 }
--- src/pharmacy/pharmacy_stock.js
+++ src/pharmacy/pharmacy_stock.js
@@ -46,7 +46,8 @@ const PharmacyStock = () => {
   return (
     <div style={{ display: "flex" }}>
       <Pharmacy_S_Navbar />
-      <Box sx={{ flexGrow: 1, p: 3, backgroundColor: "#EEF2FE", height: "90vh", marginTop: "5vh", paddingTop: "0px", paddingLeft: 0, paddingRight:0, borderTopLeftRadius: 30, borderTopRightRadius: 30, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, width: "70vw",}}>
+      <div style={{ marginLeft: "3px", flexGrow: 1, padding: "20px" }}>
+      <Box sx={{ flexGrow: 1, p: 3, backgroundColor: "#EEF2FE", height: "90vh", marginTop: "5vh", paddingTop: "0px", paddingLeft: 0, paddingRight:0, borderTopLeftRadius: 30, borderTopRightRadius: 30, borderBottomLeftRadius: 30, borderBottomRightRadius: 30, width: "80vw", margin: 'auto'}}>
         <Typography variant="h4" sx={{ fontWeight: 'bold', color: '#FFF', fontFamily: "Montserrat", width: "100%", backgroundColor:"#5889BD", borderTopLeftRadius: 30, borderTopRightRadius: 30, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, paddingBottom: '1vh', paddingTop: '1vh'}}>
           Stock Inventory
         </Typography>
@@ -122,6 +123,7 @@ const PharmacyStock = () => {
         </Box>
         </Box>
       </Box>
+      </div>
     </div>
   );
 };
